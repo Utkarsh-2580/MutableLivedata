96959c23a7e1e2adfdf111d480392eae5e69f146
utkarsh parekh <parekhutkarsh024@gmail.com>
2023-09-24 22:14:42 +0530

    Livedata with Viewmodel

--- app/src/main/java/com/example/mutablelivedata/Myviewmodel.kt
+++ app/src/main/java/com/example/mutablelivedata/Myviewmodel.kt
@@ -1,21 +1,31 @@
 package com.example.mutablelivedata
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class Myviewmodel : ViewModel() {
 
 
-    var textfirstLivedata = MutableLiveData<String>("Utkarsh Parekh")
-    var textsecondLivedata = MutableLiveData<String>("Twara Parekh")
+    var textfirstLivedataobj = MutableLiveData<String>("Utkarsh Parekh")
+    var textsecondLivedataonbj = MutableLiveData<String>("Twara Parekh")
+
+    //If we don't want to expose our data that can be overwrite then
+
+    val textfirstLivedata : LiveData<String>
+    get() = textfirstLivedataobj
+
+    val textsecondLivedata : LiveData<String>
+    get() = textsecondLivedataonbj
+
 
 
     fun getFirstText(){
-        textfirstLivedata.value = textfirst
+        textfirstLivedataobj.value = textfirst
     }
 
     fun getSecondText(){
-        textsecondLivedata.value = textsecond
+        textsecondLivedataonbj.value = textsecond
     }
 
     //In case of ViewModel
